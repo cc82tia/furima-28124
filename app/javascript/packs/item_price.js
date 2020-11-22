baf25b692d99855dba9affb51c1a7983f78abe83
cc82tia <69379810+cc82tia@users.noreply.github.com>
2020-11-22 14:44:52 +0900

    消費税計算など

--- app/javascript/packs/item_price.js
+++ app/javascript/packs/item_price.js
@@ -1,6 +1,29 @@
 window.addEventListener('load', () => {
   console.log("OK");
+
+
+  
 });
 
+window.addEventListener('load', () => {
 const priceInput = document.getElementById("item-price");
-console.log(priceInput);
+// console.log(priceInput);
+// });
+//２回CONSTできない
+// window.addEventListener('load', () => {
+// const priceInput = document.getElementById("item_price");
+
+priceInput.addEventListener('input', () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+  const tax = 0.01
+  const sumPrice = inputValue * tax
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = Math.floor(sumPrice);
+
+  const profit = document.getElementById("profit");
+  const sumProfit = inputValue * 1.01;
+  profit.innerHTML = (sumProfit);
+});
+
+})
